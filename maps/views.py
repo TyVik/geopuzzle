3513f994207ba4725edf593024ffc81dd049d193
Viktor Tyshchenko <viktor.tyshchenko@3d4medical.com>
2017-01-12 22:43:37 +0300

    Create MapForm for parameters.

--- maps/views.py
+++ maps/views.py
@@ -1,6 +1,19 @@
+import json
+
+from django import forms
+from django.http import HttpResponseBadRequest
 from django.shortcuts import render
 
-from maps.models import Meta, World
+from maps.models import Meta, Map, DIFFICULTY_LEVELS
+
+
+class MapForm(forms.Form):
+    difficulty = forms.ChoiceField(choices=DIFFICULTY_LEVELS, required=False, initial=1)
+    count = forms.IntegerField(required=False, initial=3)
+
+    def countries(self):
+        # return Map.objects.filter(meta_id=2, difficulty__gt=0, difficulty__lte=self.cleaned_data['difficulty'])[:self.cleaned_data['count']]
+        return Map.objects.filter(meta_id=2)[:5]
 
 
 def index(request):
@@ -9,7 +22,10 @@ def index(request):
 
 
 def maps(request, name):
-    countries = World.objects.filter(pk__in=[777, 778, 779, 780, 781])
+    form = MapForm(request.GET)
+    if not form.is_valid():
+        return HttpResponseBadRequest(json.dumps(form.errors))
+    countries = form.countries()
     world = Meta.objects.get(pk=2)
     meta = {'zoom': world.zoom, 'position': world.position, 'center': world.center}
     question = ', '.join([country.polygon.geojson for country in countries])
