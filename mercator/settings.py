93d31aafd7b76f193ca722de632586248dd6ca44
Viktor Tyshchenko <viktor.tyshchenko@3d4medical.com>
2017-01-14 01:24:23 +0300

    Add index page.

--- mercator/settings.py
+++ mercator/settings.py
@@ -113,3 +113,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = ['static']
--- mercator/urls.py
+++ mercator/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^maps/', include('maps.urls')),
+    url(r'^$', index, name='index'),
 ]
