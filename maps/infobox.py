ee87af355322d9fbf07e4c06c6922f684631fadd
Viktor Tyshchenko <viktor.tyshchenko@3d4medical.com>
2017-02-22 16:29:28 +0300

    Improve infobox.

--- maps/infobox.py
+++ maps/infobox.py
@@ -1,5 +1,7 @@
-from typing import Dict
+from typing import Dict, List, Set
 
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
 # from cairosvg.surface import PNGSurface
 from SPARQLWrapper import JSON
 from SPARQLWrapper import SPARQLWrapper
@@ -21,6 +23,15 @@ def query(statement: str, **kwargs) -> Dict:
                 row[field] = row[field]['value']
         return row
 
+    def get_wiki_link(instance: str, langs: List) -> Dict:
+        html = urlopen(instance)
+        soup = BeautifulSoup(html)
+        result = {}
+        for lang in langs:
+            link = soup.find("a", attrs={'hreflang': lang})
+            result[lang] = {'wiki': link.get('href'), 'name': link.get('title')}
+        return result
+
     sparql = SPARQLWrapper("https://query.wikidata.org/bigdata/namespace/wdq/sparql")
     sparql.setQuery(statement.format(**kwargs))
     sparql.setReturnFormat(JSON)
@@ -30,5 +41,9 @@ def query(statement: str, **kwargs) -> Dict:
     for row in results:
         lang = row.pop('lang')['value']
         result[lang] = prepare_row(row)
-        result[lang]['wiki'] = 'https://{language}.wikipedia.org/wiki/{name}'.format(language=lang, name=result[lang]['name'])
+
+    wiki_links = get_wiki_link(result['en']['instance'], list(result.keys()))
+    for lang, values in wiki_links.items():
+        result[lang]['wiki'] = values['wiki']
+        result[lang]['name'] = values['name']
     return result
