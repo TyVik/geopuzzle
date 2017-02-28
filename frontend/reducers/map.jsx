6ddbec0df60f98b7113a43e5a0b85ad520da7f79
Viktor Tyshchenko <viktor.tyshchenko@3d4medical.com>
2017-03-01 01:12:46 +0300

    Implement GiveUp command.

--- frontend/reducers/map.jsx
+++ frontend/reducers/map.jsx
@@ -1,5 +1,5 @@
 /* global google */
-import {GET_COUNTRIES, GET_COUNTRIES_FAIL, GET_COUNTRIES_DONE, DRAG_END_POLYGON} from "../actions";
+import {GET_COUNTRIES, GET_COUNTRIES_FAIL, GET_COUNTRIES_DONE, DRAG_END_POLYGON, GIVE_UP} from "../actions";
 
 
 function moveTo(paths, src, latLng) {
@@ -75,6 +75,18 @@ const map = (state = {...window.__INIT__, isLoaded: null, polygons: []}, action)
             return {...state, isLoaded: false};
         case GET_COUNTRIES_DONE:
             return {...state, isLoaded: true, polygons: extractPolygons(action.countries)};
+        case GIVE_UP:
+            return {...state,
+                polygons: state.polygons.map((polygon) => {
+                    if (!polygon.isSolved) {
+                        return {...polygon,
+                            draggable: false,
+                            paths: polygon.originalPath,
+                        };
+                    }
+                    return polygon
+                })
+            };
         case DRAG_END_POLYGON:
             return {...state,
                 polygons: state.polygons.map((polygon) => {
