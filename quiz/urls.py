715c54d1389c209c8d901e0fe58104dea34cbfd9
Viktor Tyshchenko <viktor.tyshchenko@3d4medical.com>
2017-03-16 05:49:41 +0300

    Append trail slash.

--- quiz/urls.py
+++ quiz/urls.py
@@ -9,6 +9,6 @@ area_patterns = [
 
 urlpatterns = [
     url(r'', include(area_patterns)),
-    url(r'^questions/(?P<name>[a-zA-Z0-9]+)', views.questions, name='quiz_questions'),
-    url(r'^(?P<name>[a-zA-Z0-9]+)', views.quiz, name='quiz_quiz'),
+    url(r'^questions/(?P<name>[a-zA-Z0-9]+)/', views.questions, name='quiz_questions'),
+    url(r'^(?P<name>[a-zA-Z0-9]+)/', views.quiz, name='quiz_quiz'),
 ]
