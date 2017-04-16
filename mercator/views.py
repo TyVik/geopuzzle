9b73d2d792e13e4163bed33767f338e7f8ce9c5b
Viktor Tyshchenko <viktor.tyshchenko@3d4medical.com>
2017-04-16 14:23:29 +0300

    Fix index page.

--- mercator/views.py
+++ mercator/views.py
@@ -13,19 +13,18 @@ def index(request: WSGIRequest) -> HttpResponse:
     query = Country.objects.language(request.LANGUAGE_CODE).filter(is_published=True).order_by('name').all()
     parts = query.filter(is_global=True)
     countries = query.filter(is_global=False)
-    games = {
-        'puzzle': {
-            'link': 'puzzle_map',
-            'rules': _('In the Puzzle you need to drag the shape of the territory to the right place. Just like in childhood we collected pictures piece by piece, so here you can collect a country from regions or whole continents from countries!')
-        },
-        'quiz': {
-            'link': 'quiz_map',
-            'rules': _('In the Quiz you need find the country by flag, emblem or the capital. Did you know that Monaco and Indonesia have the same flags? And that the flags of the United States and Liberia differ only in the number of stars? So, these and other interesting things can be learned and remembered after brainstorming right now!')
-        }
-    }
-    captions = (('puzzle', _('puzzle')), ('quiz', _('quiz')))
-    return render(request, 'index.html', {'countries': countries, 'parts': parts, 'games': games,
-                                          'captions': captions})
+    games = [{
+        'name': 'puzzle',
+        'link': 'puzzle_map',
+        'caption': _('puzzle'),
+        'rules': _('In the Puzzle you need to drag the shape of the territory to the right place. Just like in childhood we collected pictures piece by piece, so here you can collect a country from regions or whole continents from countries!')
+    }, {
+        'name': 'quiz',
+        'link': 'quiz_map',
+        'caption': _('quiz'),
+        'rules': _('In the Quiz you need find the country by flag, emblem or the capital. Did you know that Monaco and Indonesia have the same flags? And that the flags of the United States and Liberia differ only in the number of stars? So, these and other interesting things can be learned and remembered after brainstorming right now!')
+    }]
+    return render(request, 'index.html', {'countries': countries, 'parts': parts, 'games': games})
 
 
 def deprecated_redirect(request: WSGIRequest, name: str) -> HttpResponsePermanentRedirect:
