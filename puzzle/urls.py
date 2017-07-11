7f72c54641158749a727bc98fa6a0e23260c10a2
Viktor Tyshchenko <viktor.tyshchenko@3d4medical.com>
2017-07-11 15:54:42 +0300

    Allow _ in slug.

--- puzzle/urls.py
+++ puzzle/urls.py
@@ -4,6 +4,6 @@ from puzzle import views
 
 
 urlpatterns = [
-    url(r'^questions/(?P<name>[a-zA-Z0-9]+)/$', views.questions, name='puzzle_questions'),
-    url(r'^(?P<name>[a-zA-Z0-9]+)/$', views.puzzle, name='puzzle_map'),
+    url(r'^questions/(?P<name>[a-zA-Z0-9_]+)/$', views.questions, name='puzzle_questions'),
+    url(r'^(?P<name>[a-zA-Z0-9_]+)/$', views.puzzle, name='puzzle_map'),
 ]
--- quiz/urls.py
+++ quiz/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from quiz import views
 
 urlpatterns = [
-    url(r'^questions/(?P<name>[a-zA-Z0-9]+)/', views.questions, name='quiz_questions'),
-    url(r'^(?P<name>[a-zA-Z0-9]+)/', views.quiz, name='quiz_map'),
+    url(r'^questions/(?P<name>[a-zA-Z0-9_]+)/', views.questions, name='quiz_questions'),
+    url(r'^(?P<name>[a-zA-Z0-9_]+)/', views.quiz, name='quiz_map'),
 ]
