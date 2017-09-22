cb77d343a26b4631c2ecdcbeebe2c9fa5cdcdbb9
Viktor Tyshchenko <viktor.tyshchenko@3d4medical.com>
2017-09-22 19:51:21 +0300

    Remove unused code.

--- puzzle/consumer.py
+++ puzzle/consumer.py
@@ -1,30 +1,9 @@
-import json
-
-from channels.routing import route, route_class
-from channels.sessions import channel_session
+from channels.routing import route_class
 from django_redux import action
 
 from common.consumer import LanguageConsumer
 from puzzle.forms import RegionContainsForm
 from maps.models import Region
-
-
-@channel_session
-def receive(message):
-    payload = json.loads(message.content['text'])
-    if payload['type'] == 'PUZZLE_CHECK':
-        region = Region.objects.get(pk=payload['id'])
-        form = RegionContainsForm(data=payload['coords'], region=region, zoom=payload['zoom'])
-        if form.is_valid():
-            result = region.full_info(message.channel_session['lang'])
-            result['type'] = 'PUZZLE_CHECK_SUCCESS'
-            message.reply_channel.send({'text': json.dumps(result)})
-    elif payload['type'] == 'PUZZLE_GIVEUP':
-        result = {'type': 'PUZZLE_GIVEUP_DONE', 'solves': {}}
-        for id in payload['ids']:
-            region = Region.objects.get(pk=id)
-            result['solves'][id] = region.full_info(message.channel_session['lang'])
-        message.reply_channel.send({'text': json.dumps(result)})
 
 
 class PuzzleConsumer(LanguageConsumer):
