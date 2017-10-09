6bb484ac21cc59c305e5c552866b83bd6a6dbe14
Viktor Tyshchenko <viktor.tyshchenko@3d4medical.com>
2017-10-09 22:02:51 +0300

    Add auth urls.

--- mercator/urls.py
+++ mercator/urls.py
@@ -34,6 +34,7 @@ sitemaps = {
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^account/', include('users.urls')),
     url(r'^puzzle/', include('puzzle.urls')),
     url(r'^quiz/', include('quiz.urls')),
     url(r'^maps/(?P<name>[a-zA-Z0-9]+)/', views.deprecated_redirect),
---
+++ users/urls.py
@@ -0,0 +1,5 @@
+from django.conf.urls import url, include
+
+urlpatterns = [
+    url(r'', include('django.contrib.auth.urls')),
+]
