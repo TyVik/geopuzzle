3952740f9a26030df2240d471ca75826d1ea9a7e
Viktor Tyshchenko <viktor.tyshchenko@3d4medical.com>
2017-10-12 22:10:36 +0300

    Add image and language fields to User.

--- users/models.py
+++ users/models.py
@@ -1,5 +1,8 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    image = models.URLField(null=True)
+    language = models.CharField(max_length=2, default='en', choices=settings.LANGUAGES)
