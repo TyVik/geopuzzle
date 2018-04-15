d589a1ad9cb4da6d1687f76a38ae9ce4ce49d658
TyVik <tyvik8@gmail.com>
2018-04-15 21:52:52 +0300

    Typo in webpack.conf.js.

--- webpack.config.js
+++ webpack.config.js
@@ -42,16 +42,11 @@ module.exports = (env, argv) => {
             rules: [{
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
-                use: [
-                    {loader: 'babel-loader'}
-                ]
+                use: ['babel-loader']
             }, {
                 test: /\.css$/,
                 exclude: /node_modules/,
-                use: [
-                    {loader: 'style-loader'},
-                    {loader: 'css-loader', options: {importLoaders: 1}},
-                ]
+                use: ['style-loader', 'css-loader']
             }]
         },
         optimization: {
