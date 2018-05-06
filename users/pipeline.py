429018ec7fe89b035b4ac23d75fa1b8b6d5dc72f
TyVik <tyvik8@gmail.com>
2018-05-06 12:05:59 +0300

    Update pipeline for social auth.

--- users/pipeline.py
+++ users/pipeline.py
@@ -2,33 +2,49 @@ import os
 from io import FileIO
 from urllib.request import urlretrieve
 
+from django.conf import settings
 from social_core.backends.facebook import FacebookOAuth2
 from social_core.backends.google import GoogleOAuth2
 from social_core.backends.vk import VKOAuth2
 
 
 def user_details(strategy, response, backend, is_new, user=None, *args, **kwargs):
-    if user is not None and is_new:
-        image_url = None
-        user.first_name = response.get('first_name')
-        user.last_name = response.get('last_name')
-        if isinstance(backend, FacebookOAuth2):
-            value = response.get('locale', 'en_US')
-            user.language = value.split('_')[0]
-            value = response.get('picture', {'data': {'url': None}})
-            image_url = value.get('data').get('url')
-        elif isinstance(backend, VKOAuth2):
-            user.language = 'ru'
-            image_url = response.get('photo')
-        elif isinstance(backend, GoogleOAuth2):
-            user.language = response['language']
-            value = response.get('image', {'url': None})
-            image_url = value.get('url')
-            user.first_name = response['name'].get('givenName')
-            user.last_name = response['name'].get('familyName')
-
-        strategy.storage.user.changed(user)
-        if image_url:
-            result = urlretrieve(image_url)
-            user.image.save(os.path.basename(image_url), FileIO(result[0]))
+    def save_image(user, url):
+        if url:
+            result = urlretrieve(url)
+            user.image.save(os.path.basename(url), FileIO(result[0]))
             user.save()
+
+    if user is not None and is_new:
+        fields = {'first_name': None, 'last_name': None, 'language': None, 'image_url': None}
+
+        if isinstance(backend, FacebookOAuth2):
+            locale = response.get('locale', 'en_US')
+            image = response.get('picture', {'data': {'url': None}})
+            fields.update({
+                'first_name': response.get('first_name'),
+                'last_name': response.get('last_name'),
+                'language': locale.split('_')[0],
+                'image_url': image.get('data').get('url'),
+            })
+        elif isinstance(backend, VKOAuth2):
+            fields.update({
+                'first_name': response.get('first_name'),
+                'last_name': response.get('last_name'),
+                'language': 'ru',
+                'image_url': response.get('photo'),
+            })
+        elif isinstance(backend, GoogleOAuth2):
+            image = response.get('image', {'url': None})
+            fields.update({
+                'first_name': response['name'].get('givenName'),
+                'last_name': response['name'].get('familyName'),
+                'language': response['language'],
+                'image_url': image.get('url'),
+            })
+
+        user.first_name = fields['first_name']
+        user.last_name = fields['last_name']
+        user.language = fields['language'] if fields['language'] in settings.ALLOWED_LANGUAGES else 'en'
+        strategy.storage.user.changed(user)
+        save_image(user, fields['image_url'])
