a12a2c332c86b4bc02c1e7476c52711f7c2d7b1e
TyVik <tyvik8@gmail.com>
2018-05-09 10:54:40 +0300

    Disable Sentry for local development.

--- mercator/settings/development.py
+++ mercator/settings/development.py
@@ -3,7 +3,9 @@ from mercator.settings.settings import *
 STATIC_ROOT = 'collected_static'
 ALLOWED_HOSTS = ('*',)
 
+RAVEN_CONFIG = {}
+
 LOGGING['loggers']['django.db.backends'].update({
     'handlers': ['console'],
-    'level': 'DEBUG'
+    'level': 'ERROR'
 })
