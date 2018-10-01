32e249026f514bfec098919df8678da6fbff58f6
TyVik <tyvik8@gmail.com>
2018-10-01 21:01:54 +0300

    Add tests for Congratulation.

---
+++ frontend/__tests__/Congratulation.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import Congratulation from '../components/Congratulation';
+
+
+describe('shallow <Congratulation /> components', () => {
+  let props = {result: 'result', url: 'url'};
+  let wrapper;
+  window.__CONGRATULATION__ = {text: 'text'};
+
+  beforeEach(() => {
+    wrapper = shallow(<Congratulation {...props}/>);
+  });
+
+  it('render', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('click close', () => {
+    wrapper.instance().onClose();
+    expect(wrapper.html()).toBe('');
+  });
+});
