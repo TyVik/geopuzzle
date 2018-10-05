f93d67f28b14ec55183b26adc2caf0dd9b204b0f
TyVik <tyvik8@gmail.com>
2018-10-05 21:24:00 +0300

    Pass SonarQube stage if no any associated pull request.

--- .circleci/sonarqube.sh
+++ .circleci/sonarqube.sh
@@ -11,14 +11,16 @@ fi
 if [ "$CIRCLE_BRANCH" = "develop" ]; then
   SONAR_OPTS="${SONAR_OPTS} -Dsonar.analysis.mode=publish"
 else
-  PR=$(echo -n $CIRCLE_PULL_REQUEST | tail -c 2)
-  SONAR_OPTS="${SONAR_OPTS} -Dsonar.pullrequest.key=${PR}"
-  SONAR_OPTS="${SONAR_OPTS} -Dsonar.pullrequest.base=develop"
-  SONAR_OPTS="${SONAR_OPTS} -Dsonar.pullrequest.branch=${CIRCLE_BRANCH}"
-  SONAR_OPTS="${SONAR_OPTS} -Dsonar.pullrequest.endpoint=https://api.github.com/"
-  SONAR_OPTS="${SONAR_OPTS} -Dsonar.pullrequest.provider=GitHub"
-  SONAR_OPTS="${SONAR_OPTS} -Dsonar.pullrequest.github.repository=TyVik/geopuzzle"
-  SONAR_OPTS="${SONAR_OPTS} -Dsonar.pullrequest.github.token.secured=${GITHUB_TOKEN}"
+  if [ "$CIRCLE_PULL_REQUEST" != "" ]; then
+    PR=$(echo -n $CIRCLE_PULL_REQUEST | tail -c 2)
+    SONAR_OPTS="${SONAR_OPTS} -Dsonar.pullrequest.key=${PR}"
+    SONAR_OPTS="${SONAR_OPTS} -Dsonar.pullrequest.base=develop"
+    SONAR_OPTS="${SONAR_OPTS} -Dsonar.pullrequest.branch=${CIRCLE_BRANCH}"
+    SONAR_OPTS="${SONAR_OPTS} -Dsonar.pullrequest.endpoint=https://api.github.com/"
+    SONAR_OPTS="${SONAR_OPTS} -Dsonar.pullrequest.provider=GitHub"
+    SONAR_OPTS="${SONAR_OPTS} -Dsonar.pullrequest.github.repository=TyVik/geopuzzle"
+    SONAR_OPTS="${SONAR_OPTS} -Dsonar.pullrequest.github.token.secured=${GITHUB_TOKEN}"
+  fi
 fi
 
 echo $SONAR_OPTS
