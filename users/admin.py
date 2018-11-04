fc95ab0df6c20bc2961902f045d2fb3c335f9477
TyVik <tyvik8@gmail.com>
2018-11-04 21:50:13 +0300

    Add fields to User admin list.

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
+from django.utils.safestring import SafeText
 
 from users.models import User
 
 
 @admin.register(User)
 class PuzzleAdmin(ModelAdmin):
-    pass
+    list_display = ('id', 'image_tag', 'username', 'is_active', 'language')
+    list_display_links = ('image_tag', 'username')
+
+    def image_tag(self, obj):
+        return SafeText(f'<img src="{obj.image.name}"/>')
