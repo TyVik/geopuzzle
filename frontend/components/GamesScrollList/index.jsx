c8789236c9b6b37a07e89c1c5183d9dbb55f0811
TyVik <tyvik8@gmail.com>
2019-03-03 23:11:39 +0300

    Extract GameScrollList component.

---
+++ frontend/components/GamesScrollList/index.jsx
@@ -0,0 +1,65 @@
+'use strict';
+import React from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import Loading from "../Loading";
+import {Col} from "react-bootstrap";
+
+
+export default class GameScrollList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = GameScrollList.getInitialState(props);
+  }
+
+  componentDidMount() {
+    if (this.state.page === 0) {
+      this.fetchPage(1, false).then();
+    }
+  }
+
+  static getInitialState(props) {
+    return {items: [], page: 0, hasMore: true, url: props.url};
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (this.props.url !== prevProps.url) {
+      this.setState(GameScrollList.getInitialState(this.props), () => {this.fetchPage(1, false).then()});
+    }
+  }
+
+  fetchPage = async (page, replace) => {
+    let url = new URL(this.props.url);
+    url.searchParams.append('page', page);
+    try {
+      let response = await fetch(url.toString(), {method: 'GET'});
+      let data = await response.json();
+      let hasMore = data.length === 30;
+      let items = replace ? data : this.state.items.concat(data);
+      this.setState({...this.state, items: items, page: page, hasMore: hasMore});
+    } catch {
+      this.setState({...this.state, page: page - 1, hasMore: false});
+    }
+  };
+
+  fetchNextPage = () => {
+    this.fetchPage(this.state.page + 1, false).then();
+  };
+
+  renderItem = (item) => {
+    return <Col md={3} sm={4} xs={6} xl={2} className="my-2 item-container" key={item.url}>
+      <a href={item.url}>
+        <i className="created_by">by {item.user}</i>
+        <img className="img-fluid rounded" src={item.image} alt={item.name}/>
+      </a>
+      <div className="text-center">{item.name}</div>
+    </Col>;
+  };
+
+  render() {
+    let items = this.state.items;
+    return <InfiniteScroll dataLength={items.length} children={items} next={this.fetchNextPage} className="row"
+                           hasMore={this.state.hasMore} loader={<Loading/>}>
+      {items.map(this.renderItem)}
+    </InfiniteScroll>;
+  }
+}
--- frontend/workshop/index.jsx
+++ frontend/workshop/index.jsx
@@ -1,71 +1,27 @@
 'use strict';
 import React from "react";
-import InfiniteScroll from 'react-infinite-scroll-component';
-import Loading from "../components/Loading/index";
 import {FormattedMessage as Msg} from 'react-intl';
+import GameScrollList from "../components/GamesScrollList";
 
 
 class Workshop extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {puzzles: [], page: 0, search: '', tag: 0, hasMore: true};
+    this.state = {search: '', searchArg: '', tag: 0};
   }
-
-  componentDidMount() {
-    this.fetchPage(1, false);
-  }
-
-  fetchPage(page, replace) {
-    let url = `${location.pathname}items/?page=${page}`;
-    if (this.state.search) {
-      url += `&search=${this.state.search}`;
-    }
-    if (this.state.tag > 0) {
-      url += `&tag=${this.state.tag}`;
-    }
-    fetch(url, {method: 'GET'})
-      .then(response => response.json())
-      .then(data => {
-        let hasMore = data.length === 30;
-        if (replace) {
-          this.setState({...this.state, puzzles: data, page: page, hasMore: hasMore});
-        } else {
-          let items = this.state.puzzles.concat(data);
-          this.setState({...this.state, puzzles: items, page: page, hasMore: hasMore});
-        }
-      })
-      .catch(() => {
-        this.setState({...this.state, page: page - 1, hasMore: false});
-      });
-  }
-
-  fetchNextPage = () => {
-    this.fetchPage(this.state.page + 1, false);
-  };
 
   onChange = (event) => {
     let value = event && event.target.value || '';
     if (this.timeout !== null) {
       clearTimeout(this.timeout);
     }
-    this.timeout = setTimeout(() => {this.fetchPage(1, true)}, 300);
-    this.setState({...this.state, search: value, hasMore: true});
+    this.timeout = setTimeout(() => {this.setState({...this.state, searchArg: value})}, 300);
+    this.setState({...this.state, search: value});
   };
 
   onChangeTag = (event) => {
-    this.setState({...this.state, tag: Number(event.target.value), hasMore: true},
-      () => this.fetchPage(1, true));
+    this.setState({...this.state, tag: Number(event.target.value)});
   };
-
-  render_map(puzzle) {
-    return  <div className="col-md-3 col-sm-4 col-6 col-xl-2 my-2 item-container" key={puzzle.url}>
-      <a href={puzzle.url}>
-        <i className="created_by">by {puzzle.user}</i>
-        <img className="img-fluid rounded" src={puzzle.image} alt={puzzle.name}/>
-      </a>
-      <div className="text-center">{puzzle.name}</div>
-    </div>;
-  }
 
   render_controls() {
     return <form className="row justify-content-between">
@@ -87,17 +43,23 @@ class Workshop extends React.Component {
     </form>;
   }
 
-  render() {
-    if (this.state.page === 0) {
-      return <Loading/>;
+  currentUrl() {
+    let params = new URLSearchParams();
+    if (this.state.searchArg) {
+      params.set('search', this.state.searchArg);
     }
-    let puzzles = this.state.puzzles;
+    if (this.state.tag > 0) {
+      params.set('tag', this.state.tag);
+    }
+    let result = params.toString();
+    result = result.length > 0 ? `${location.href}items/?${result}` : `${location.href}items/`;
+    return result;
+  }
+
+  render() {
     return <React.Fragment>
       {this.render_controls()}
-      <InfiniteScroll dataLength={puzzles.length} children={puzzles} next={this.fetchNextPage} className="row"
-                             hasMore={this.state.hasMore} loader={<Loading/>}>
-        {puzzles.map(this.render_map)}
-      </InfiniteScroll>
+      <GameScrollList url={this.currentUrl()}/>
     </React.Fragment>;
   }
 }
