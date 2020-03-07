1a28cc4305b14b24f9012fc079404287df79ca8a
TyVik <tyvik8@gmail.com>
2020-03-07 12:21:47 +0300

    Fix CircleCI.

--- mercator/settings/circleci.py
+++ mercator/settings/circleci.py
@@ -4,3 +4,6 @@ from .settings import *
 STATIC_ROOT = 'collected_static'
 
 LOGGING['handlers'].pop('file', None)
+LOGGING['handlers'].pop('commands', None)
+LOGGING['loggers'].pop('commands', None)
+LOGGING['loggers'].pop('wambachers', None)
