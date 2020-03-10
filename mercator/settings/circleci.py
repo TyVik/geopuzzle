033dff078ed181d497fa481b8be8d944d5637d20
TyVik <tyvik8@gmail.com>
2020-03-11 01:03:54 +0300

    Prepare environment for CircleCI.

--- mercator/settings/circleci.py
+++ mercator/settings/circleci.py
@@ -2,8 +2,3 @@ from .settings import *
 
 
 STATIC_ROOT = 'collected_static'
-
-LOGGING['handlers'].pop('file', None)
-LOGGING['handlers'].pop('commands', None)
-LOGGING['loggers'].pop('commands', None)
-LOGGING['loggers'].pop('wambachers', None)
