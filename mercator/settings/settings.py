d50db039cf08825aea8225c64a9bbb20242e69d2
Viktor Tyshchenko <tyvik8@gmail.com>
2021-10-09 08:25:33 +0300

    Update dependencies. (#712)

--- mercator/settings/settings.py
+++ mercator/settings/settings.py
@@ -93,11 +93,11 @@ TEMPLATES = [{
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_USER_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': os.environ.get('POSTGRES_PORT', 5432),
         'ATOMIC_REQUESTS': True,
     }
 }
